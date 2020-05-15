d6d69330db399de96b2a4320b237f97c1d4f7e51
abhijeet <abhijeetvermayash@gmail.com>
2020-05-15 19:59:39 +0530

    Added OrderDetails node in firebase database

--- app/src/main/java/com/example/sonisteel/Cart.java
+++ app/src/main/java/com/example/sonisteel/Cart.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,33 +25,72 @@ public class Cart extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
-        DatabaseReference dbr= FirebaseDatabase.getInstance().getReference().child("CurrentOrder").child("Order");
-
-        final TextView textview1,textView2;
-        textview1=findViewById(R.id.txt);
-        textView2=findViewById(R.id.txt2);
+        final DatabaseReference dr=FirebaseDatabase.getInstance().getReference().child("OrderDetails");
 
 
 
+        Intent i=getIntent();
+       final int noofkadhai=i.getIntExtra("STAINLESSKADHAI",0);
+        final int noofhandi=i.getIntExtra("HANDI",0);
+        final int nooftops=i.getIntExtra("SETOFTOPS",0);
+
+        final EditText name,add,phone;
+        final Button button;
+
+        name=findViewById(R.id.e1);
+        add=findViewById(R.id.e2);
+        phone=findViewById(R.id.e3);
+        button=findViewById(R.id.btn);
 
 
-
-        dbr.addValueEventListener(new ValueEventListener() {
-           @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-
-
-
-
-
-            }
-
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onClick(View v) {
+                int a=0;
+                OrderDetails orderDetails=new OrderDetails();
+                orderDetails.setName(name.getText().toString());
+                orderDetails.setAdd(add.getText().toString());
+                orderDetails.setPhone(Integer.parseInt(phone.getText().toString()));
+
+                HashMap<String,Integer> hashMap=new HashMap<>();
+
+                if(noofkadhai!=0)
+                {
+                    hashMap.put("Stain less Kadhai",noofhandi);
+
+                    a=a+noofkadhai*200;
+                }
+
+                if(noofhandi!=0)
+                {
+                    hashMap.put("Handi with Copper base",noofhandi);
+                    a=a+noofhandi*300;
+                }
+                if(nooftops!=0)
+                {
+                    hashMap.put("Set Of Tops",nooftops);
+                    a=a+nooftops*800;
+                }
+
+                orderDetails.setHm(hashMap);
+                orderDetails.setTotal(a);
+
+                dr.child("Order").setValue(orderDetails);
 
             }
         });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/sonisteel/OrderDetails.java
+++ app/src/main/java/com/example/sonisteel/OrderDetails.java
@@ -8,4 +8,49 @@ public class OrderDetails {
     private String add;
     private int phone;
     private HashMap<String,Integer> hm;
+    private int Total;
+
+    public OrderDetails() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotal() {
+        return Total;
+    }
+
+    public void setTotal(int total) {
+        Total = total;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public HashMap<String, Integer> getHm() {
+        return hm;
+    }
+
+    public void setHm(HashMap<String, Integer> hm) {
+        this.hm = hm;
+    }
 }
+
